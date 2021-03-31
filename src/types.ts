e2ca07c6fc2e85390ef0505e2dc7c753d60621ee
Liam Cooper <liamcoop1997@gmail.com>
2021-03-31 08:52:27 -0700

    added job information to types.ts, prisma schema file.

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,14 @@ export interface Todo {
   text: string;
   completed: boolean;
 }
+
+export interface jobPosting {
+  id: string;
+  createdAt: string;
+  applied: boolean;
+  company: string;
+  title: string;
+  description: string;
+  contact: string;
+  notes: string;
+}
