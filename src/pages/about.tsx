6d070fff1e3900e17ab4863409199f4d3d8a43c4
Liam Cooper <liamcoop1997@gmail.com>
2021-03-31 21:51:43 -0700

    Further refactoring jobItem layout, css.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,10 +11,6 @@ const About: NextPage = () => {
   if (error != null) return <div>Error loading jobs...</div>
   if (jobs == null) return <div>Loading...</div>
 
-  if (jobs.length === 0) {
-    return <div className={styles.emptyState}>Add a job</div>
-  }
-
   return (
     <div>
       <Head>
@@ -22,9 +18,11 @@ const About: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.jobFoldDiv}><AddJobFold /></div>
+        <div className={styles.jobFoldDiv}>
+          <AddJobFold />
+        </div>
         {jobs.map((job) => (
-          <JobItem job={job} />
+          <JobItem key={job.id} job={job} />
         ))}
       </main>
 
@@ -36,49 +34,63 @@ const JobItem: React.FC<{ job: Job }> = ({ job }) => {
   const [showMore, setShowMore] = useState(false);
 
   return (
-    <div className={styles.jobItem}>
-      <div className={styles.jobMain}>
-        <p>{`Applied: ${job.applied ? '✔' : '✕'}`}</p>
-        <p>{`Title: ${job.title}`}</p>
-        <p>{`Company: ${job.company}`}</p>
-        <p>{`Contact: ${job.contact}`}</p>
-        <p onClick={() => setShowMore(!showMore)}>⌄</p>
-        <div className={styles.jobButtons}>
-          <button 
-            className={styles.deleteButton} 
-            onClick={() => deleteJob(job.id)}
-          >
-            ✕
-          </button>
-          <button 
-            className={styles.editButton} 
-          >
-            Edit
-          </button>
+    <>
+      <div className={styles.jobGrid}>
+        <div className={styles.appliedDiv}>
+          <div className={styles.circle}>
+            <p className={styles.jobAppliedSymbol}>{job.applied ? '✔' : '✕'}</p>
+          </div>
+          <p className={styles.jobAppliedText}>{`${job.applied ? '' : 'Not'} Applied`}</p>
         </div>
+        <p className={styles.jobTitle}>{job.title}</p>
+        <div className={styles.jobFlexbox}>
+          <p className={styles.jobCompany}>{job.company}</p>
+          <p className={styles.jobLocation}>{job.location}</p>
+          {job.datePosted ? 
+            <p className={styles.jobDatePosted}>{job.datePosted}</p> : null}
+        </div>
+        <button 
+          className={styles.deleteButton} 
+          onClick={() => deleteJob(job.id)}
+        >
+          x
+        </button>
+        <p 
+          className={styles.carat} 
+          onClick={() => setShowMore(!showMore)}
+        >
+          ⌄
+        </p>
       </div>
       {showMore && 
-        <div className={styles.jobMore}>
-          <p>{job.description}</p>
-          <p>{job.notes}</p>
-        </div>
+        <div className={styles.jobShowMore}>
+          {job.link ?
+            <a className={styles.jobLink} href={job.link} target="_blank">
+              {job.link} 
+            </a> : null}
+          {job.description ? 
+            <p className={styles.jobDescription}>{job.description}</p> : null}
+          {job.notes ? 
+            <p className={styles.notes}>{job.notes}</p> : null}
+        </div> 
       }
-    </div>
+    </>
   );
 }
 
 const AddJobFold = () => {
   const [show, setShow] = useState(false);
   return (
-    <div>
-      <div className={styles.plusGroup}>
+    <div className={styles.addJobFoldContainer}>
+      <div className={styles.addJobIcon}>
         <div 
           onClick={() => setShow(!show)} 
           className={styles.circle}
+          style={{ cursor: 'pointer' }}
         >
           <p>+</p>
         </div>
-        <p>Click to add new job</p>
+        <p>Add a job</p>
       </div>
       {show && <AddJobItem closeFold={() => {setShow(false)}} /> }
     </div>
@@ -92,6 +104,11 @@ const AddJobItem = ({ closeFold }) => {
   const [applied, setApplied] = useState(false)
   const [notes, setNotes] = useState('')
   const [contact, setContact] = useState('')
+  const [datePosted, setDatePosted] = useState('');
+  const [dateClosed, setDateClosed] = useState('');
+  const [location, setLocation] = useState('');
+  const [link, setLink] = useState('');
+  // const [tags, setTags] = useState([]);
 
   const resetJob = () => {
     setTitle('')
@@ -100,16 +117,24 @@ const AddJobItem = ({ closeFold }) => {
     setApplied(false)
     setNotes('')
     setContact('')
+    setDatePosted('');
+    setDateClosed('');
+    setLocation('');
+    setLink('');
   }
-  const fireCreateJob = () => 
-    createJob(
-      title, 
-      description, 
-      company, 
-      applied, 
-      contact, 
-      notes
-    );
+  const fireCreateJob = () => createJob(
+    title, 
+    description, 
+    company, 
+    applied, 
+    contact, 
+    notes,
+    datePosted,
+    dateClosed,
+    location,
+    link,
+  );
+
   return (
     <form
       className={styles.addJob}
@@ -121,45 +146,69 @@ const AddJobItem = ({ closeFold }) => {
       }}
     >
       <div>
-        <label htmlFor="applied">Applied</label>
         <input 
           type="checkbox" 
           checked={applied}
           value="applied"
           onChange={e => setApplied(!applied)}
         />
+        <label htmlFor="applied">Applied</label>
       </div>
       <input 
-        className={styles.input + styles.inputCompany}
-        placeholder="Company Name"
+        className={`${styles.input} ${styles.inputCompany}`}
+        placeholder="Company"
         value={company}
         onChange={e => setCompany(e.target.value)}
       />
       <input 
-        className={styles.input + styles.inputTitle}
-        placeholder="Job Title"
+        className={`${styles.input} ${styles.inputTitle}`}
+        placeholder="Title"
         value={title}
         onChange={e => setTitle(e.target.value)}
       />
       <input 
-        className={styles.input + styles.inputDescription}
-        placeholder="Job Description"
-        value={description}
-        onChange={e => setDescription(e.target.value)}
-      />
-      <input 
-        className={styles.input + styles.inputNotes}
-        placeholder="Additional notes"
+        className={`${styles.input} ${styles.inputNotes}`}
+        placeholder="Notes"
         value={notes}
         onChange={e => setNotes(e.target.value)}
       />
       <input 
-        className={styles.input + styles.inputContact}
-        placeholder="Contact Info"
+        className={`${styles.input} ${styles.inputContact}`}
+        placeholder="Contact"
         value={contact}
         onChange={e => setContact(e.target.value)}
       />
-      <button className={styles.button}>submit</button>
+      <input 
+        className={`${styles.input} ${styles.inputDatePosted}`}
+        placeholder="Post Date"
+        value={datePosted}
+        onChange={e => setDatePosted(e.target.value)}
+      />
+      <input 
+        className={`${styles.input} ${styles.inputDateClosed}`}
+        placeholder="Close Date"
+        value={dateClosed}
+        onChange={e => setDateClosed(e.target.value)}
+      />
+      <input 
+        className={`${styles.input} ${styles.inputLocation}`}
+        placeholder="Location"
+        value={location}
+        onChange={e => setLocation(e.target.value)}
+      />
+      <input 
+        className={`${styles.input} ${styles.link}`}
+        placeholder="Link"
+        value={link}
+        onChange={e => setLink(e.target.value)}
+      />
+      <input 
+        className={`${styles.input} ${styles.inputDescription}`}
+        placeholder="Description"
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <button className={styles.addJobButton}>Add Job</button>
     </form>
 
   );
