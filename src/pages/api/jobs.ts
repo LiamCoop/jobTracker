4f8644d65e8ddf9ddccc816e6be837e9c69d5ccd
Liam Cooper <liamcoop1997@gmail.com>
2021-04-02 10:16:34 -0700

    Prisma instantiation rewritten as singleton to prevent opening so many clients.

--- src/pages/api/jobs.ts
+++ src/pages/api/jobs.ts
@@ -1,7 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import { prisma } from '../../lib/utils/prisma';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
