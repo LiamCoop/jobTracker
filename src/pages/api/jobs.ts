a260a2fa5fa6944c67d04d4631fc09e821a4053b
Liam Cooper <liamcoop1997@gmail.com>
2021-04-02 15:22:07 -0700

    Found prisma docs for dealing w/ too many connection issues.

--- src/pages/api/jobs.ts
+++ src/pages/api/jobs.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-import { prisma } from '../../lib/utils/prisma';
+import prisma from '../../lib/utils/prisma';
 
 // const prisma = new PrismaClient();
 
--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from '../../lib/utils/prisma';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
