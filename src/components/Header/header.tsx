fa7485e7a442de59e0efde915913125c86772497
Liam Cooper <liamcoop1997@gmail.com>
2021-04-04 21:47:40 -0700

    Factored User component back into header. Needed user for conditional rendering links.

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,35 +2,36 @@ import React, { useState } from 'react';
 import styles from './Header.module.css';
 import { useUser } from '@auth0/nextjs-auth0';
 
-const User: React.FC = () => {
+export const Header = () => {
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  // user.sub contains unique idea 
-  return (
-    user ? (
-      <div className={styles.userContainer}>
-        <img 
-          className={styles.userAvatar} 
-          src={user.picture} 
-          alt={user.name} 
-        />
-        <p className={styles.userName}>{user.name}</p>
-        <p className={styles.userEmail}>{user.email}</p>
-      </div>
-    ) : null
-  );
-}
 
-export const Header = () => (
-  <div className={styles.headerContainer}>
-    <div className={styles.linkContainer}>
-      <a href="/api/auth/login">Login</a>
-      <a href="/api/auth/logout?returnTo=http%3A@2F@2Flocalhost:3000">
-        Logout
-      </a>
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.linkContainer}>
+        {user ? (
+          <a href="/api/auth/logout?returnTo=http%3A@2F@2Flocalhost:3000">
+            Logout
+          </a>
+        ) : (
+          <a href="/api/auth/login">Login</a>
+        )}
+      </div>
+      <div className={styles.userContainer}>
+        {user ? (
+          <>
+            <img 
+              className={styles.userAvatar} 
+              src={user?.picture} 
+              alt={user?.name} 
+            />
+            <p className={styles.userName}>{user?.name}</p>
+            <p className={styles.userEmail}>{user?.email}</p>
+          </>
+        ) : null}
+      </div>
     </div>
-    <User />
-  </div>
-)
+  )
+}
