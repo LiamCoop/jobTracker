e1242d91ef027b633705fb4fd677213fc4bf6ca9
Liam Cooper <liamcoop1997@gmail.com>
2021-04-06 12:00:06 -0700

    Rewrote a lot of livesearch. Added ability to filter by tags, text, and have things propagate correctly.

--- src/components/LiveSearch/liveSearch.tsx
+++ src/components/LiveSearch/liveSearch.tsx
@@ -1,29 +1,56 @@
-import React, { useState} from "react";
-import styles from './liveSearch.module.css';
+import React, { useEffect, useState } from "react";
+import styles from './LiveSearch.module.css';
+import { JobItem } from '../Job/job';
+import { Job } from '../../types';
 
-export const LiveSearch: React.FC<{ children: React.ReactNodeArray}> = 
-({ children }) => {
+
+
+export const LiveSearch: React.FC<{ jobs: Job[]}> = 
+({ jobs }) => {
+  const [show, setShow] = useState(jobs);
   const [text, setText] = useState('')
+  const [tag, setTag] = useState(null);
 
+  // adjust if a tag is clicked (to show those with that tag)
+  const searchTag = (argtag: string) => {
+    setTag(tag !== argtag ? argtag : null)
+  }
+
+  // if tag, text (search params) change
+  // refine tags first, then refine by text
+  useEffect(() => {
+    let tryshow = jobs;
+    if(tag) {
+      tryshow = tryshow.filter((job: Job) => 
+        job.tags.filter((jobtag) => jobtag === tag).length)
+    }
+    if(text) {
+      tryshow = tryshow.filter((job: Job) => Object.values(job).join()
+        .toLowerCase().includes(text.toLowerCase()))
+    }
+    setShow(tryshow)
+  }, [tag, text, jobs])
+
+  
   return(
     <div className={styles.liveSearchContainer}>
       <div className={styles.searchBar}>
-        <button type="submit" className={styles.searchButton}>
-          <svg className={styles.svg} height="24px" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-          </svg>
-        </button>
         <input
           className={styles.searchTerm}
           value={text}
           placeholder="Search Jobs"
           onChange={e => setText(e.target.value)}
         />
+        <button className={styles.searchButton}>
+          <svg className={styles.svg} height="24px" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+          </svg>
+        </button>
       </div>
       <div className={styles.jobContainer}>
-        {children.map((child: React.ReactElement) => Object.values(child.props.job)
-          .join().toLowerCase().includes(text.toLowerCase()) ? 
-            child : null)}
+        {show.map((job) => 
+          <JobItem job={job} key={job.id} searchTag={searchTag} />
+        )}
       </div>
     </div>
   )
