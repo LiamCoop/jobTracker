14053af28acc62f1e68739599aaeb93c30a3881c
Liam Cooper <liamcoop1997@gmail.com>
2021-04-05 21:00:43 -0700

    CSS & display layout changes. Trying to reduce amount of stylings. Reduce complexity, build a clean, responsive UI.

--- src/components/Job/job.tsx
+++ src/components/Job/job.tsx
@@ -8,7 +8,7 @@ export const JobItem: React.FC<{ job: Job }> = ({ job }) => {
   const [showMore, setShowMore] = useState(false);
   
   return (
-    <>
+    <div className={styles.jobContainer}>
       <div className={styles.jobGrid}>
         <div className={styles.jobAppliedDiv}>
           <div 
@@ -24,60 +24,46 @@ export const JobItem: React.FC<{ job: Job }> = ({ job }) => {
           <p className={styles.jobAppliedText}>{`${job.applied ? '' : 'Not '}Applied`}</p>
         </div>
         <p className={styles.jobTitle}>{job.title}</p>
-        {job.company || job.location || job.datePosted || job.link ? (
-          <div className={styles.jobBottom}>
-            <div className={styles.jobFlexbox}>
-              {job.company ? 
-                <p className={styles.jobCompany}>{job.company}</p> : null}
-              {job.location ? 
-                <p className={styles.jobLocation}>{job.location}</p> : null}
-            </div>
-            {job.link ?
-              <a 
-                className={styles.jobLink} 
-                href={job.link} 
-                target="_blank"
-              >🔗</a> : null}
-          </div> 
-        ) : null}
+        <div className={styles.jobBottom}>
+          <div className={styles.jobFlexbox}>
+            {job.company ? 
+              <p className={styles.jobCompany}>{job.company}</p> : null}
+            {job.location ? 
+              <p className={styles.jobLocation}>{job.location}</p> : null}
+            {job.datePosted ? 
+              <p className={styles.jobdatePosted}>{job.datePosted}</p> : null}
+          </div>
+          <div onClick={() => setShowMore(!showMore)} className={styles.chevronDiv} >
+            <ChevronSVG />
+          </div>
+        </div> 
         <div className={styles.jobButtonDiv}>
           <div onClick={() => deleteJob(job.id)} className={styles.circleDelete} >
             <DeleteSVG />
           </div>
         </div>
-        <div onClick={() => setShowMore(!showMore)} className={styles.chevronDiv} >
-          <ChevronSVG />
+        <div className={styles.jobLinkDiv}>
+          {job.link ? <a 
+            className={styles.jobLink} 
+            href={job.link} 
+            target="_blank"
+          >🔗</a> : null}
         </div>
       </div>
       {showMore && 
         <div className={styles.jobShowMore}>
           {job.tags.length ? <TagDisplay job={job} /> : null}
-          <ShowMore 
-            datePosted={job.datePosted} 
-            description={job.description} 
-            notes={job.notes} 
-          />
+          {job.description ? 
+            <p className={styles.jobDescription}>
+              {job.description}
+            </p> : null}
+          {job.notes ? 
+            <p className={styles.notes}>{job.notes}</p> : null}
         </div>
       }
-    </>
+    </div>
   );
 }
-
-const ShowMore: React.FC<{ 
-  datePosted: string, 
-  description: string, 
-  notes: string
-}> = ({ datePosted, description, notes}) => (
-  <>
-    {datePosted ? 
-      <p className={styles.jobDatePosted}>{datePosted}</p> : null} 
-    {description ? 
-      <p className={styles.jobDescription}>{description}</p> : null}
-    {notes ? 
-      <p className={styles.notes}>{notes}</p> : null}
-  </> 
-
-)
 
 const TagDisplay: React.FC<{ job: Job }>= ({ job }) => (
  <div className={styles.tagsDiv}>
