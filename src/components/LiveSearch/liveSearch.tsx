7b0104abe783ace218705f7ea1c6139d4aac0b77
Liam Cooper <liamcoop1997@gmail.com>
2021-04-06 17:12:46 -0700

    Width styling updates across application.

--- src/components/LiveSearch/liveSearch.tsx
+++ src/components/LiveSearch/liveSearch.tsx
@@ -10,6 +10,7 @@ export const LiveSearch: React.FC<{ jobs: Job[]}> =
   const [show, setShow] = useState(jobs);
   const [text, setText] = useState('')
   const [tag, setTag] = useState(null);
+  const [tags, setTags] = useState(jobs.flatMap((job: Job) => job.tags));
 
   // adjust if a tag is clicked (to show those with that tag)
   const searchTag = (argtag: string) => {
@@ -34,18 +35,25 @@ export const LiveSearch: React.FC<{ jobs: Job[]}> =
   
   return(
     <div className={styles.liveSearchContainer}>
-      <div className={styles.searchBar}>
-        <input
-          className={styles.searchTerm}
-          value={text}
-          placeholder="Search Jobs"
-          onChange={e => setText(e.target.value)}
-        />
-        <button className={styles.searchButton}>
-          <svg className={styles.svg} height="24px" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-          </svg>
-        </button>
+      <div className={styles.searchGroup}>
+        <div className={styles.searchBar}>
+          <input
+            className={styles.searchTerm}
+            value={text}
+            placeholder="Search Jobs"
+            onChange={e => setText(e.target.value)}
+          />
+          <button className={styles.searchButton}>
+            <svg className={styles.svg} height="24px" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+            </svg>
+          </button>
+        </div>
+        {tag && <div className={styles.tagContainer}>
+          <div className={styles.tag} onClick={() => searchTag(tag)}>
+            <p className={styles.tagText}>{tag}</p>
+          </div>
+        </div>}
       </div>
       <div className={styles.jobContainer}>
         {show.map((job) => 
