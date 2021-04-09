cbce86c6b8838c7bf5b8d475b48563b27502bb0b
Liam Cooper <liamcoop1997@gmail.com>
2021-04-08 17:58:50 -0700

    commented out prismaclient import to see if this solves railway problem or not.

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 import prisma from '../../lib/utils/prisma';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
