0bb75c3c7ddfcb4ebcbb6744f96b2204eb123790
Liam Cooper <liamcoop1997@gmail.com>
2021-04-08 20:09:06 -0700

    file swaps to place the About page at application root.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,56 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import React from "react";
 import styles from "../styles/Home.module.css";
+import { useJobs } from '../api2';
+import { AddJobFold } from '../components/AddJob/addJob';
+import { Header } from '../components/Header/header';
+import { LiveSearch } from '../components/LiveSearch/liveSearch';
+import { useUser } from '@auth0/nextjs-auth0';
 
-const Home: NextPage = () => {
+const About: NextPage = () => {
+  const { user } = useUser();
+
+  const { data: jobs, error } = useJobs(user?.sub);
+
+  if (error != null) return <div>Error loading jobs...</div>
+  if (jobs == null) return <div>Loading...</div>
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Railway NextJS Prisma</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>About</title>
+        <meta key="cacheControl" http-equiv='cache-control' content='no-cache' />
+        <meta key="httpEquiv" http-equiv='expires' content='0' />
+        <meta key="httpEquiv2" http-equiv='pragma' content='no-cache' />
       </Head>
-
-      <header className={styles.header}>
-        <h1 className={styles.title}>Todos</h1>
-        <h2 className={styles.desc}>
-          NextJS app connected to Postgres using Prisma and hosted on{" "}
-          <a href="https://railway.app">Railway</a>
-        </h2>
-      </header>
+      
 
       <main className={styles.main}>
-        <h1>test</h1>
+        {user ? 
+        <>
+          <Header />
+          <AddJobFold />
+          <div className={styles.liveSearchContainer}>
+            <LiveSearch jobs={jobs} />
+          </div>
+        </> : <Land />}
       </main>
+
     </div>
   );
-};
+}
 
-export default Home;
+const Land: React.FC = () => {
+  return (
+    <div>
+      <a href="/api/auth/login">
+        <div className={styles.linkDiv}>
+          <h1 className={styles.login}>Login</h1>
+        </div>
+      </a>
+    </div>
+  )
+}
+
+export default About;
