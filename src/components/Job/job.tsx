ff6346d04207cc9ca5f544bec1997894e8f81195
Liam Cooper <liamcoop1997@gmail.com>
2021-04-10 01:29:56 -0700

    Added editable fields for notes / contacts. May include for other fields later.

--- src/components/Job/job.tsx
+++ src/components/Job/job.tsx
@@ -73,18 +73,65 @@ export const JobItem: React.FC<{
           {job.notes ? 
             <>
               <h1 className={styles.header}>Notes</h1>
-              <p className={styles.jobNotes}>{job.notes}</p>
+              <Editable item={job.notes} update={(arg: string) => {
+                updateJob({ ...job, notes: arg }) }} 
+              />
             </> : null}
           {job.contact ? 
             <>
               <h1 className={styles.header}>Contact</h1>
-              <p className={styles.jobContact}>{job.contact}</p>
+              <Editable item={job.contact} update={(arg: string) => {
+                updateJob({ ...job, contact: arg })}} 
+              />
             </> : null}
         </div>
       }
     </div>
   );
 }
+
+const Editable: React.FC<{ item: string, update: (arg: string) => void }> = ({ item, update }) => {
+  const [editing, setEditing] = useState(false);
+  const [itemVal, setItemVal] = useState(item);
+  return (
+    <>
+      {!editing ? (
+        <div className={styles.editDiv}>
+          <p className={styles.jobNotes}>{itemVal}</p>
+          <button 
+            className={styles.editSave} 
+            onClick={() => setEditing(true)}
+          >
+            <svg height="24px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+            </svg>
+          </button>
+        </div> 
+      ) : (
+        <div className={styles.editDiv}>
+          <input 
+            className={styles.editInput}
+            value={itemVal}
+            onChange={e => setItemVal(e.target.value)}
+          />
+          <button 
+            className={styles.editSave}
+            onClick={() => {
+              setEditing(false)
+              update(itemVal)
+            }}
+          >
+            <svg height="24px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+            </svg>
+          </button>
+        </div>  
+        )
+      }
+    </>
+  )
+}
+
 
 const TagDisplay: React.FC<{ 
   job: Job,
